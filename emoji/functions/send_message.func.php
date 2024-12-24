0c9748c1f868c8567545462c90d5d071d9f9d2db
Iam-sree <iamsreeganesh87@gmail.com>
2024-12-24 09:04:54 +0530

    Add files via upload

---
+++ emoji/functions/send_message.func.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include '../includes/insert.class.php';
+
+$conn_Insert = new Insert_class();
+
+if(isset($_REQUEST['send-btn'])){
+
+$message = $_REQUEST['type-message']; 
+$sender_id = $_SESSION['id'];
+$reciever_id = $_REQUEST['contact_id'];
+
+$insert_message = $conn_Insert->add_message($message,$sender_id,$reciever_id);
+
+if($insert_message){
+
+echo  "<script></script>"; /*alert('Message Sent!');*/
+echo "<script>window.location.href='../index.php?contact_id=$reciever_id';</script>";
+
+}
+
+else{
+
+    echo  "<script>alert('failed!');</script>";
+    echo  "<script>window.location.href='../index.php';</script>";
+    
+    }
+}
+
+
+?>
